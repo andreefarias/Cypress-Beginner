731e16fa67e9270954311b259fd2fcced7a00242
andreefarias <andre_luiz_08@hotmail.com>
2024-02-09 12:34:29 -0300

    CY 1 - Add and update testcases

---
+++ cypress/e2e/Privacy.cy.js
@@ -0,0 +1,5 @@
+it('17 - Acessa a página da política de privacidade removendo o target e então clicando no link', function () {
+    cy.visit('./src/privacy.html')
+    
+    cy.contains('Talking About Testing').should('be.visible')
+  })
